88c8f27707abd5a9ffc172995ca860aacdd115b4
Jesper Persson <mrjesperpersson@hotmail.se>
2020-12-14 13:15:31 +0100

    Add project files.

---
+++ BasgruppsInlamning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasgruppsInlamning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
