71a10a8d6a9d8f08d3829dd5566a083dfc01a91d
Jesper Persson <mrjesperpersson@hotmail.se>
2020-12-14 13:25:29 +0100

    Created Class Human and a List of 9 humans

---
+++ BasgruppsInlamning/Human.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Cache;
+using System.Text;
+using System.Windows.Markup;
+
+namespace BasgruppsInlamning
+{
+    class Human
+    {
+        private string name;
+        private int age;
+        private string birthday;
+        private string favouriteFood;
+        private string favouriteBand;
+        private string favouriteMovie;
+        private string loves;
+        private string hates;
+        private string superPower;
+        private string zodiac;
+        private string reasonToPrograming;
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public string Birthday { get => birthday; set => birthday = value; }
+        public string FavouriteFood { get => favouriteFood; set => favouriteFood = value; }
+        public string FavouriteBand { get => favouriteBand; set => favouriteBand = value; }
+        public string FavouriteMovie { get => favouriteMovie; set => favouriteMovie = value; }
+        public string Loves { get => loves; set => loves = value; }
+        public string Hates { get => hates; set => hates = value; }
+        public string SuperPower { get => superPower; set => superPower = value; }
+        public string Zodiac { get => zodiac; set => zodiac = value; }
+        public string ReasonToPrograming { get => reasonToPrograming; set => reasonToPrograming = value; }
+
+        public Human()
+        {
+
+        }
+        public Human(string name, int age, string birthday, string favouriteFood, string favouriteBand, string favouriteMovie, string loves,
+            string hates, string superPower, string zodiac, string reasonToPrograming)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Birthday = birthday;
+            this.FavouriteFood = favouriteFood;
+            this.FavouriteBand = favouriteBand;
+            this.FavouriteMovie = favouriteMovie;
+            this.Loves = loves;
+            this.Hates = hates;
+            this.SuperPower = superPower;
+            this.Zodiac = zodiac;
+            this.ReasonToPrograming = reasonToPrograming;
+
+        }
+
+  
+
+
+
+        public void Describe()
+        {
+            Console.Clear();
+            Console.WriteLine($"|{Name}|");
+            Console.WriteLine($" Ålder: {Age}");
+            Console.WriteLine($" Födelsedagsdag: {Birthday}");
+            Console.WriteLine($" Favoritmat: {FavouriteFood}");
+            Console.WriteLine($" Favoritband: {FavouriteBand}");
+            Console.WriteLine($" Favoritfilm: {FavouriteMovie}");
+            Console.WriteLine($" {Name} älskar: {Loves}");
+            Console.WriteLine($" {Name} hatar: {Hates}");
+            Console.WriteLine($" Stjärntecken: {Zodiac}");
+            Console.WriteLine($" Superpower: {SuperPower}");
+            Console.WriteLine($" Anledning till att programmera: {ReasonToPrograming}");
+        }
+        public void Change()
+        {
+
+            Console.WriteLine($"1. Namn");
+            Console.WriteLine($"2. Ålder");
+            Console.WriteLine($"3. Födelsedagsdag");
+            Console.WriteLine($"4. Favoritmat");
+            Console.WriteLine($"5. Favoritband");
+            Console.WriteLine($"6. Favoritband");
+            Console.WriteLine($"7. Älskar");
+            Console.WriteLine($"8. Hatar");
+            Console.WriteLine($"9. Stjärntecken");
+            Console.WriteLine($"10. Superpower");
+            Console.WriteLine($"11. Anledning till att programmera");
+        }
+
+    }
+
+}
--- BasgruppsInlamning/Program.cs
+++ BasgruppsInlamning/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Microsoft.VisualBasic.FileIO;
 
 namespace BasgruppsInlamning
 {
     class Program
     {
+        public static List<Human> groupList = new List<Human>();
         static void Main(string[] args)
         {
           
+        }
+        
+
+        static void AddMembersToList()
+
+        {
+
+            groupList.Add(new Human("MA Lin", 29, "13 mars", "Råkaka", "Modest Mouse", "Snatch", "Höst/vinter", "Banan", "Mrs.Hindsight", "Fisk", "Skapande"));
+            groupList.Add(new Human("Andrie", 26, "12 januari", "Carbonara", "Two Steps from Hell", "The Lord of the Rings", "Resa", "Oliver", "Instant olive detection", "Stenbock", "Problemlösning"));
+            groupList.Add(new Human("Jesper", 28, "29 maj", "Lammracks", "Lars Winnerbäck", "Catch me if you can", "De enkla sakerna", "Folk som filmar på konserter", "Teleportering", "Tvilling", "Frihet"));
+            groupList.Add(new Human("Leroy", 32, "22 oktober", "Pizza", "We the best music", "Matrix", "Choklad", "Kaviar", "Flyga", "Våg", "Dynamisk"));
+            groupList.Add(new Human("Gurra M", 33, "17 november", "Tacos", "Berliner Philharmoniker", "Any Star Wars :) ", "J.S. Bach", "Schlager", "Tala med hundvalpar", "Skorpion", "Problemlösning"));
+            groupList.Add(new Human("Jonna", 30, "5 februari", "Pizza", "Full of hell", "Interstellar", "Musik", "Kött", "Odödlig", "Vattumann", "Tjurskallig"));
+            groupList.Add(new Human("Linus", 27, "17 juli", "Fisksoppa", "Jack Moy", "The secret life of Walter Mitty", "Banan", "Senap", "Andas under vatten", "Kräfta", "Få ett riktigt skoj jobb"));
+            groupList.Add(new Human("Nils", 21, "15 mars", "Pannkakor", "Beach Boys", "American Psycho", "höst/vinter", "Snö", "Slask", "Fisk", "Roligt"));
+            groupList.Add(new Human("Yulrok", 38, "23 april", "Musli", "Morcheeba", "The green book", "Ost", "Slöseri", "Tankeöverföring", "Oxen", "Biljett till \"digital nomad\" - livet"));
+
         }
     }
 }
